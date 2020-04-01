05bbfc2dc86e3a1c956c84a2abb0acb96f12cd84
lijin <electrixoul@outlook.com>
2020-04-01 20:42:03 +0800

    增加了观察模式

--- uart.cpp
+++ uart.cpp
@@ -43,97 +43,110 @@ int main(int argc, char *argv[]) {
     {
         system("clear");
 
+        MS.reset_command();
+
         char console_input;
         cout<<endl<<"================= 输入EL-AI模块功能配置项 ================="<<endl;
 
-        cout<<"直接发送目标选择触发指令 ? (y/n)"<<endl;
+        cout<<"是否进入观察模式 ? (y/n)"<<endl;
         cin>>console_input;
         if(console_input == 'y')
         {
-            MS.reset = false;
-            MS.centerSelection = true;
-            MS.power_off = false;
-            MS.NoRecover = true;
-            MS.NaiveMode = true;
-            MS.trigger = true;
+            MS.LoiterMode = true;
         }
         else
         {
-            cout<<endl<<"是否重置 ? (y/n)"<<endl;
+            MS.LoiterMode = false;
+
+            cout<<"直接发送目标选择触发指令 ? (y/n)"<<endl;
             cin>>console_input;
             if(console_input == 'y')
             {
-                MS.reset = true;
-                MS.power_off = false;
-            }
-            else {
                 MS.reset = false;
-                MS.power_off = false;
-            }
-
-            cout<<endl<<"是否进入简单模式 ? (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-                MS.NaiveMode = true;
-            else MS.NaiveMode = false;
-
-            cout<<endl<<"简单模式是否采用预设ROI选择 ? (输入n则采用ROI框选初始化) "<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-                MS.NaiveMode_ClickSelection = true;
-            else MS.NaiveMode_ClickSelection = false;
-
-            cout<<endl<<"追踪车还是行人 ? (输入 y 则进入车辆检测模式)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-                MS.ped_or_car = false;
-            else MS.ped_or_car = true;
-
-            cout<<endl<<"是否关闭目标自动重找回 ? (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-                MS.NoRecover = true;
-            else MS.NoRecover = false;
-
-            cout<<endl<<"是否采用画面准星选取目标 ? (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
                 MS.centerSelection = true;
-            else MS.centerSelection = false;
-
-            cout<<endl<<"设置本次目标跟踪触发信号: (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
+                MS.power_off = false;
+                MS.NoRecover = true;
+                MS.NaiveMode = true;
                 MS.trigger = true;
-            else MS.trigger = false;
-
-            cout<<endl<<"设备是否关机? (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-            {
-                /// 关机指令:
-                /// reset -> false
-                /// power_off -> true
-                MS.reset = false;
-                MS.power_off = true;
             }
             else
             {
-                MS.power_off = false;
-            }
+                cout<<endl<<"是否重置 ? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                {
+                    MS.reset = true;
+                    MS.power_off = false;
+                }
+                else {
+                    MS.reset = false;
+                    MS.power_off = false;
+                }
 
-            cout<<endl<<"是否执行软重启? (y/n)"<<endl;
-            cin>>console_input;
-            if(console_input == 'y')
-            {
-                /// 软重启指令:
-                /// reset -> true
-                /// power_off -> true
-                MS.reset = true;
-                MS.power_off = true;
-            }
-            else {
+                cout<<endl<<"是否进入简单模式 ? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.NaiveMode = true;
+                else MS.NaiveMode = false;
 
+                cout<<endl<<"简单模式是否采用预设ROI选择 ? (输入n则采用ROI框选初始化) "<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.NaiveMode_ClickSelection = true;
+                else MS.NaiveMode_ClickSelection = false;
+
+                cout<<endl<<"追踪车还是行人 ? (输入 y 则进入车辆检测模式)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.ped_or_car = false;
+                else MS.ped_or_car = true;
+
+                cout<<endl<<"是否关闭目标自动重找回 ? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.NoRecover = true;
+                else MS.NoRecover = false;
+
+                cout<<endl<<"是否采用画面准星选取目标 ? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.centerSelection = true;
+                else MS.centerSelection = false;
+
+                cout<<endl<<"设置本次目标跟踪触发信号: (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                    MS.trigger = true;
+                else MS.trigger = false;
+
+                cout<<endl<<"设备是否关机? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                {
+                    /// 关机指令:
+                    /// reset -> false
+                    /// power_off -> true
+                    MS.reset = false;
+                    MS.power_off = true;
+                }
+                else
+                {
+                    MS.power_off = false;
+                }
+
+                cout<<endl<<"是否执行软重启? (y/n)"<<endl;
+                cin>>console_input;
+                if(console_input == 'y')
+                {
+                    /// 软重启指令:
+                    /// reset -> true
+                    /// power_off -> true
+                    MS.reset = true;
+                    MS.power_off = true;
+                }
+                else {
+
+                }
             }
         }
 
